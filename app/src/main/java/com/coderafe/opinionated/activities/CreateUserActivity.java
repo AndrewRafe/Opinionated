7985c068a6d8ae818bc0c371e45597f2e7e95b1c
AndrewRafe <andrew.rafe@outlook.com>
2016-09-29 13:42:28 +1000

    attempted to add create user code

--- app/src/main/java/com/coderafe/opinionated/activities/CreateUserActivity.java
+++ app/src/main/java/com/coderafe/opinionated/activities/CreateUserActivity.java
@@ -8,7 +8,17 @@ import android.widget.EditText;
 
 import com.coderafe.opinionated.R;
 
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
 public class CreateUserActivity extends AppCompatActivity {
+
+    public static final String URL_ADD_USER_STRING = "http://www.coderafe.net/add_user.php";
 
     private EditText mUserEmail;
     private EditText mUserPassword;
@@ -29,11 +39,54 @@ public class CreateUserActivity extends AppCompatActivity {
     }
 
     /**
+     * TODO: Make it an ASYNC Task implementation
      * On click method for the create user view that will attempt to post the information
      * to the database
      * @param view A reference to the create user view
      */
     public void postData(View view) {
 
+        String email = mUserEmail.getText().toString();
+        String password = mUserPassword.getText().toString();
+        String yearOfBirth = mUserYearOfBirth.getText().toString();
+        String postcode = mPostcode.getText().toString();
+        String gender = mGender.getText().toString();
+
+        String data = "";
+
+        try {
+            data = URLEncoder.encode("email", "UTF-8") + "=" +
+                    URLEncoder.encode(email, "UTF-8");
+            data += URLEncoder.encode("password", "UTF-8") + "=" +
+                    URLEncoder.encode(password, "UTF-8");
+            data += URLEncoder.encode("yearOfBirth", "UTF-8") + "=" +
+                    URLEncoder.encode(yearOfBirth, "UTF-8");
+            data += URLEncoder.encode("postcode", "UTF-8") + "=" +
+                    URLEncoder.encode(postcode, "UTF-8");
+            data += URLEncoder.encode("gender", "UTF-8") + "=" +
+                    URLEncoder.encode(gender, "UTF-8");
+        } catch (UnsupportedEncodingException exception) {
+
+        }
+
+
+
+        try {
+
+            URL url = new URL(URL_ADD_USER_STRING);
+
+            //Send Post Data Request
+            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("POST");
+            urlConnection.setDoOutput(true);
+            OutputStreamWriter wr = new OutputStreamWriter(urlConnection.getOutputStream());
+            wr.write(data);
+            wr.flush();
+
+        } catch (MalformedURLException malformedURLException) {
+
+        } catch (IOException ioException) {
+
+        }
     }
 }
