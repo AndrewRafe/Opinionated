e8a95bef87ef31deecda708643fc75452218bc96
AndrewRafe <andrew.rafe@outlook.com>
2016-09-29 12:50:32 +1000

    Create user and login activity layouts Navigation between title and login and create user

--- app/src/main/java/com/coderafe/opinionated/activities/CreateUserActivity.java
+++ app/src/main/java/com/coderafe/opinionated/activities/CreateUserActivity.java
@@ -1,15 +1,39 @@
 package com.coderafe.opinionated.activities;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import com.coderafe.opinionated.R;
 
 public class CreateUserActivity extends AppCompatActivity {
 
+    private EditText mUserEmail;
+    private EditText mUserPassword;
+    private EditText mUserYearOfBirth;
+    private EditText mPostcode;
+    private EditText mGender;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
+
+        mUserEmail = (EditText) findViewById(R.id.create_user_email_et);
+        mUserPassword = (EditText) findViewById(R.id.create_user_password_et);
+        mUserYearOfBirth = (EditText) findViewById(R.id.create_user_year_of_birth_et);
+        mPostcode = (EditText) findViewById(R.id.create_user_postcode_et);
+        mGender = (EditText) findViewById(R.id.create_user_gender_et);
+    }
+
+    /**
+     * On click method for the create user view that will attempt to post the information
+     * to the database
+     * @param view A reference to the create user view
+     */
+    public void postData(View view) {
+
     }
 }
--- app/src/main/java/com/coderafe/opinionated/activities/TitleActivity.java
+++ app/src/main/java/com/coderafe/opinionated/activities/TitleActivity.java
@@ -1,7 +1,9 @@
 package com.coderafe.opinionated.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.coderafe.opinionated.R;
 
@@ -11,5 +13,23 @@ public class TitleActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * An on click method for the create user button that launches the create user activity
+     * @param view A reference to the title activity view
+     */
+    public void createUser(View view) {
+        Intent intent = new Intent(this, CreateUserActivity.class);
+        startActivity(intent);
+    }
+
+    /**
+     * On click method for the log in button that launches the log in activity
+     * @param view A reference to the title view
+     */
+    public void logIn(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
